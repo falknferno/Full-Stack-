0e3fca87133c8e14738a20bbcf803b81f8fb0a5b
Anthony Perkins <falknferno81@gmail.com>
2023-09-22 08:44:54 -0500

    More CSharp Learning

---
+++ 2ndApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Tony_Perkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello\n");
+            Console.Write("What's your name?");
+            Console.ReadKey();
+        }
+    }
+}
