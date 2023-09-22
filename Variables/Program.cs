a34646b03008401ed1cae8168bbe3d6a64a70cab
Anthony Perkins <falknferno81@gmail.com>
2023-09-22 09:36:49 -0500

    Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 24;
+            int y = 12;
+            float z = 6;
+            char t = '+';
+            string s = x.ToString();
+            
+            s = z.ToString();
+            s = t.ToString();
+            s = y.ToString();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
