a4637ed083f7ab1bbe2b7705eabdd5e753df0a6e
Anthony Perkins <falknferno81@gmail.com>
2023-09-22 09:04:36 -0500

    Name

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Dear " + name + " Welcome to our lesson");
+            Console.ReadKey();
+        }
+    }
+}
