e276364bd52bcdd1d0d940d402d7e2af685fa519
Anthony Perkins <falknferno81@gmail.com>
2023-09-27 01:52:20 -0500

    While DoWhile and Watch

---
+++ WinForm7/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            //int loopWorkAmount = 0, loopControlValue = 10;
+            //while (loopControlValue < 20)
+            //{
+                //textBox1.Text += loopWorkAmount.ToString() + Environment.NewLine;
+                //loopControlValue++;
+                //loopWorkAmount++;
+            //}
+            //MessageBox.Show(loopWorkAmount.ToString());
+
+            // nested loop
+            textBox1.Clear();
+            int inLoopWorkAmount = 0, inLoopControlValue = 0;
+            int outLoopControlValue = 10, outLoopWorkAmount = 0;
+            while (outLoopControlValue < 20)
+            {
+                inLoopControlValue = 0;
+                while (inLoopControlValue<8)
+                {
+                    inLoopControlValue++;
+                    inLoopWorkAmount++;
+                    textBox1.Text += outLoopControlValue.ToString() + " " + inLoopControlValue.ToString() +
+                        Environment.NewLine;
+                }
+                outLoopControlValue++;
+                outLoopWorkAmount++;
+            }
+            MessageBox.Show("outLoopWorkAmount works " + outLoopWorkAmount.ToString() +
+                " times. inLoopWorkAmount works " + inLoopWorkAmount.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int loopWorkAmount = 0, loopControlValue = 10;
+            do
+            {
+                loopControlValue++;
+                loopWorkAmount++;
+                textBox1.Text += loopWorkAmount.ToString() + Environment.NewLine;
+                
+            }
+            while (loopControlValue < 20);
+                MessageBox.Show(loopWorkAmount.ToString());
+        }
+    }
+}
