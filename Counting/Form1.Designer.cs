4d30d95ea7c7d2931bb61c5fdc9e064e66e2bca2
Anthony Perkins <falknferno81@gmail.com>
2023-09-27 14:46:24 -0500

    Counting WinForm Project

---
+++ Counting/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Counting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStartFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbDivisibleTerm = new System.Windows.Forms.ComboBox();
+            this.txtDivisibleNumbers = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.rbBlack = new System.Windows.Forms.RadioButton();
+            this.chBold = new System.Windows.Forms.CheckBox();
+            this.chItalic = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 130);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(394, 130);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To";
+            // 
+            // txtStartFrom
+            // 
+            this.txtStartFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStartFrom.Location = new System.Drawing.Point(192, 130);
+            this.txtStartFrom.Name = "txtStartFrom";
+            this.txtStartFrom.Size = new System.Drawing.Size(100, 26);
+            this.txtStartFrom.TabIndex = 1;
+            this.txtStartFrom.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtStartFrom_KeyPress);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.Location = new System.Drawing.Point(430, 130);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(100, 26);
+            this.txtTo.TabIndex = 3;
+            this.txtTo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTo_KeyPress);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCount.Location = new System.Drawing.Point(272, 277);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(130, 41);
+            this.btnCount.TabIndex = 8;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(430, 277);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(130, 41);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 210);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Divisible Number";
+            // 
+            // cmbDivisibleTerm
+            // 
+            this.cmbDivisibleTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDivisibleTerm.FormattingEnabled = true;
+            this.cmbDivisibleTerm.Location = new System.Drawing.Point(239, 209);
+            this.cmbDivisibleTerm.Name = "cmbDivisibleTerm";
+            this.cmbDivisibleTerm.Size = new System.Drawing.Size(121, 28);
+            this.cmbDivisibleTerm.TabIndex = 5;
+            this.cmbDivisibleTerm.SelectedIndexChanged += new System.EventHandler(this.cmbDivisibleTerm_SelectedIndexChanged);
+            // 
+            // txtDivisibleNumbers
+            // 
+            this.txtDivisibleNumbers.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtDivisibleNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDivisibleNumbers.Location = new System.Drawing.Point(0, 364);
+            this.txtDivisibleNumbers.Multiline = true;
+            this.txtDivisibleNumbers.Name = "txtDivisibleNumbers";
+            this.txtDivisibleNumbers.Size = new System.Drawing.Size(1370, 290);
+            this.txtDivisibleNumbers.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbGreen);
+            this.groupBox1.Controls.Add(this.rbBlue);
+            this.groupBox1.Controls.Add(this.rbRed);
+            this.groupBox1.Controls.Add(this.rbBlack);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(618, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 277);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Color";
+            // 
+            // rbGreen
+            // 
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(17, 100);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(71, 21);
+            this.rbGreen.TabIndex = 0;
+            this.rbGreen.Text = "Green";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            this.rbGreen.CheckedChanged += new System.EventHandler(this.rbGreen_CheckedChanged);
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(17, 73);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(58, 21);
+            this.rbBlue.TabIndex = 0;
+            this.rbBlue.Text = "Blue";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            this.rbBlue.CheckedChanged += new System.EventHandler(this.rbBlue_CheckedChanged);
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Location = new System.Drawing.Point(17, 46);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(55, 21);
+            this.rbRed.TabIndex = 0;
+            this.rbRed.Text = "Red";
+            this.rbRed.UseVisualStyleBackColor = true;
+            this.rbRed.CheckedChanged += new System.EventHandler(this.rbRed_CheckedChanged);
+            // 
+            // rbBlack
+            // 
+            this.rbBlack.AutoSize = true;
+            this.rbBlack.Checked = true;
+            this.rbBlack.Location = new System.Drawing.Point(17, 19);
+            this.rbBlack.Name = "rbBlack";
+            this.rbBlack.Size = new System.Drawing.Size(65, 21);
+            this.rbBlack.TabIndex = 0;
+            this.rbBlack.TabStop = true;
+            this.rbBlack.Text = "Black";
+            this.rbBlack.UseVisualStyleBackColor = true;
+            this.rbBlack.CheckedChanged += new System.EventHandler(this.rbBlack_CheckedChanged);
+            // 
+            // chBold
+            // 
+            this.chBold.AutoSize = true;
+            this.chBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chBold.Location = new System.Drawing.Point(94, 265);
+            this.chBold.Name = "chBold";
+            this.chBold.Size = new System.Drawing.Size(64, 24);
+            this.chBold.TabIndex = 6;
+            this.chBold.Text = "Bold";
+            this.chBold.UseVisualStyleBackColor = true;
+            this.chBold.CheckedChanged += new System.EventHandler(this.chBold_CheckedChanged);
+            // 
+            // chItalic
+            // 
+            this.chItalic.AutoSize = true;
+            this.chItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chItalic.Location = new System.Drawing.Point(94, 295);
+            this.chItalic.Name = "chItalic";
+            this.chItalic.Size = new System.Drawing.Size(67, 24);
+            this.chItalic.TabIndex = 7;
+            this.chItalic.Text = "Italic";
+            this.chItalic.UseVisualStyleBackColor = true;
+            this.chItalic.CheckedChanged += new System.EventHandler(this.chItalic_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 654);
+            this.Controls.Add(this.chItalic);
+            this.Controls.Add(this.chBold);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDivisibleNumbers);
+            this.Controls.Add(this.cmbDivisibleTerm);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtStartFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Counting Numbers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStartFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbDivisibleTerm;
+        private System.Windows.Forms.TextBox txtDivisibleNumbers;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.RadioButton rbBlack;
+        private System.Windows.Forms.CheckBox chBold;
+        private System.Windows.Forms.CheckBox chItalic;
+    }
+}
+
---
+++ Counting/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        int firstNumber = 0, lastNumber = 0;
+        int divisibleTerm = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDivisibleTerm.Items.Add("2");
+            cmbDivisibleTerm.Items.Add("3");
+            cmbDivisibleTerm.Items.Add("4");
+            cmbDivisibleTerm.Items.Add("5");
+            cmbDivisibleTerm.Items.Add("6");
+            cmbDivisibleTerm.Items.Add("7");
+            cmbDivisibleTerm.Items.Add("8");
+            cmbDivisibleTerm.Items.Add("10");
+        }
+
+        private void cmbDivisibleTerm_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            divisibleTerm = Convert.ToInt32(cmbDivisibleTerm.SelectedItem);
+        }
+        int controlNumber = 1;
+
+        private void txtStartFrom_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void rbBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbBlack.Checked)
+            {
+                rbRed.Checked = false;
+                rbBlack.Checked = false;
+                rbGreen.Checked = false;
+                txtDivisibleNumbers.ForeColor = Color.Black;
+            }
+        }
+
+        private void rbRed_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbRed.Checked)
+            {
+                rbBlack.Checked = false;
+                rbBlue.Checked = false;
+                rbGreen.Checked = false;
+                txtDivisibleNumbers.ForeColor = Color.Red;
+            }
+        }
+
+        private void rbBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbBlue.Checked)
+            {
+                rbBlack.Checked = false;
+                rbRed.Checked = false;
+                rbGreen.Checked = false;
+                txtDivisibleNumbers.ForeColor = Color.Blue;
+            }
+        }
+
+        private void rbGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbGreen.Checked)
+            {
+                rbBlack.Checked= false;
+                rbBlue.Checked= false;
+                rbRed.Checked= false;
+                txtDivisibleNumbers.ForeColor = Color.Green;
+            }
+        }
+
+        private void chBold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chBold.Checked)
+                txtDivisibleNumbers.Font = new Font(txtDivisibleNumbers.Font.FontFamily, txtDivisibleNumbers.Font.Size, FontStyle.Bold);
+            else
+                txtDivisibleNumbers.Font = new Font(txtDivisibleNumbers.Font.FontFamily, txtDivisibleNumbers.Font.Size, FontStyle.Regular);
+
+
+        }
+
+        private void chItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chItalic.Checked)
+                txtDivisibleNumbers.Font = new Font(txtDivisibleNumbers.Font.FontFamily, txtDivisibleNumbers.Font.Size, FontStyle.Italic);
+            else
+                txtDivisibleNumbers.Font = new Font(txtDivisibleNumbers.Font.FontFamily, txtDivisibleNumbers.Font.Size, FontStyle.Regular);
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+             string divisibleNumber = ""; //will always change if loop is running
+            // if error pops up due to empty fields
+            if (txtStartFrom.Text.Trim() == "" || txtTo.Text.Trim() == "")
+            {
+                MessageBox.Show("please fill the neccessary fields");  //screen will pop up when trying to run, instead of error on here
+            }
+            else if (cmbDivisibleTerm.SelectedIndex == -1)
+            {
+                MessageBox.Show("please select divisible term");
+            }
+            else
+            {
+                firstNumber = Convert.ToInt32(txtStartFrom.Text);
+                lastNumber = Convert.ToInt32(txtTo.Text);
+                //MessageBox.Show("From " + firstNumber + " To " +  lastNumber); // start, to, count
+                //MessageBox.Show("Divisible " + divisibleTerm + " From " + firstNumber + " To " + lastNumber); // start, to, divisible, count
+                for (int i = firstNumber; i < lastNumber; i++)
+                {
+                    if (i % divisibleTerm == 0)
+                    {
+                        divisibleNumber += i.ToString() + " "; // hold true numbers to write to the textbox end of the loop
+                        if (controlNumber % 10 == 0)
+                            divisibleNumber += Environment.NewLine;
+                        controlNumber++;
+                    }
+
+                }
+                txtDivisibleNumbers.Text = divisibleNumber;
+            }
+        }
+    }
+}
