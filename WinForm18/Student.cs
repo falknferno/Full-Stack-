e4e34167a20353f03cb4b49e26e77485242f23d5
Anthony Perkins <falknferno81@gmail.com>
2023-09-28 18:17:29 -0500

    Static

---
+++ WinForm18/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm18
+{
+    public class Student
+    {
+        public static int StudentNumber { get; set; }
+        public static void getAddNote() // static method name
+            //don't need a code to access getAddNote, but can add method 
+        {
+            MessageBox.Show("Static");
+        }
+    }
+}
