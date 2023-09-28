15811743b03190b1ecb8bb77969908d15f44512d
Anthony Perkins <falknferno81@gmail.com>
2023-09-28 18:40:01 -0500

    Inheritance

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Employee : Human
+    {
+        protected string Position {  get; set; }
+        public double Salary { get; set; }
+        public void Setvalues()
+        {
+            Name = "Tony";
+            ID = 2;
+            Gender = 'M';
+            Age = 42;
+        }
+    }
+}
---
+++ Inheritance/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Teacher : Employee
+    {
+        public string Branch {  get; set; }
+        public void FindSalary()
+        {
+            Salary = 2500;
+        }
+        public void Setvalues2()
+        {
+            Position = "Teacher";
+            Branch = "Math";
+        }
+    }
+}
