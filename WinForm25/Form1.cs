cde07ce3e6cb0d47dc9e9c0c1853a7892beafcb4
Anthony Perkins <falknferno81@gmail.com>
2023-09-29 15:48:22 -0500

    Delegates

---
+++ WinForm25/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate void fillTextBox(int x);
+        string textvalue = "";
+        
+        public void FillMathNote(int ExamNote)
+        {
+            textvalue += "Your Math Note is: " + ExamNote + Environment.NewLine;
+        }
+        public void FillChemistryNote(int ExamNote)
+        {
+            textvalue += "Your Chemistry Note is: " + ExamNote + Environment.NewLine;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void ShowinTexBox()
+        {
+            textBox1.Text = textvalue;
+        }
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            fillTextBox filltext = new fillTextBox(FillMathNote);
+            filltext += FillChemistryNote;
+            filltext(80);
+            filltext -= FillMathNote;
+            filltext(60);
+            ShowinTexBox();
+        }
+    }
+}
